687da20d8287dccfb9a1b01065b0ff5a8c92adc1
pranithan-kang <pranithan_kang@hotmail.com>
2021-06-11 00:14:27 +0700

    3 files modified

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const btnOpenWindow = document.getElementById('btnOpenWindow');
 // Profile elements
 const email = document.getElementById('email');
 const userId = document.getElementById('userId');
-const pictureUrl = document.getElementById('pictureUrl');
+const profilePicture = document.getElementById('profilePicture');
 const displayName = document.getElementById('displayName');
 const statusMessage = document.getElementById('statusMessage');
 
@@ -32,11 +32,25 @@ async function main() {
   osInfoTextElem.textContent = JSON.stringify(liff);
 
   let osInfo = liff.getOS();
-  switch(osInfo) {
-    case "android": body.style.background = "#d1f5d3"; break;
-    case "ios": body.style.background = "#eeeeee"; break;
-    case "web": body.style.background = "#110fee"; break;
+  switch (osInfo) {
+    case 'android':
+      body.style.background = '#d1f5d3';
+      break;
+    case 'ios':
+      body.style.background = '#eeeeee';
+      break;
+    case 'web':
+      body.style.background = '#110fee';
+      break;
   }
-
 }
+
+async function getProfile() {
+  let profile = liff.getProfile();
+  profilePicture.src = profile.pictureUrl;
+  userId.innerHTML = `<b>User Id:</b> ${profile.userId}`;
+  statusMessage.innerHTML = `<b>Status Message:</b> ${profile.statusMessage}`;
+  displayName.innerHTML = `<b>Display Name:</b> ${profile.displayName}`;
+}
+
 main();
